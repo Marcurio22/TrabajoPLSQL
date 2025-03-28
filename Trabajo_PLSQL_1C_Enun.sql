cb7f25ab585b69f269cceb8893f8567799072929
Marcurio22 <mzl1004@alu.ubu.es>
2025-03-28 12:09:56 +0100

    Verificación de que el segundo plato se encuentre disponible
    El SELECT comprobará que se haya añadido un arg_id_segundo_plato y que se encuentre disponible. En caso de no estarlo, se lanzará el error: -20001

--- Trabajo_PLSQL_1C_Enun.sql
+++ Trabajo_PLSQL_1C_Enun.sql
@@ -73,6 +73,16 @@ create or replace procedure registrar_pedido(
         END IF;
     END IF;
     
+    -- Verificar disponibilidad del segundo plato
+    IF arg_id_segundo_plato IS NOT NULL THEN
+        SELECT disponible INTO plato_disponible 
+        FROM platos 
+        WHERE id_plato = arg_id_segundo_plato;
+        IF plato_disponible = 0 THEN
+            RAISE_APPLICATION_ERROR(-20001, 'Uno de los platos seleccionados no está disponible.');
+        END IF;
+    END IF;
+    
     
 end;
 /
